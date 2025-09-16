ba49d6027e1d7f5b5c288fa82b1ea441c928ccf3
maathor <marc@agorapulse.com>
2025-09-16 14:52:45 +0200

    fix: Improve zone validation to handle null region/zones cases

--- variables.tf
+++ variables.tf
@@ -88,7 +88,7 @@ variable "zones" {
   default     = []
 
   validation {
-    condition = (var.region == null && length(var.zones) == 0) || (var.region != null && length(var.zones) == 0) || (var.region == null && length(var.zones) > 0 && length(distinct([for zone in var.zones : substr(zone, 0, length(zone) - 2)])) <= 1) || (var.region != null && length(var.zones) > 0 && alltrue([for zone in var.zones : startswith(zone, var.region)]))
+    condition     = (var.region == null && length(var.zones) == 0) || (var.region != null && length(var.zones) == 0) || (var.region == null && length(var.zones) > 0 && length(distinct([for zone in var.zones : substr(zone, 0, length(zone) - 2)])) <= 1) || (var.region != null && length(var.zones) > 0 && alltrue([for zone in var.zones : startswith(zone, var.region)]))
     error_message = "When both region and zones are specified, all zones must belong to the specified region. When only zones are specified, all zones must belong to the same region."
   }
 }
